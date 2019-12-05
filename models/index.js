023ef605adda6bbd604b9cc03f90cf7517b90a7c
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-12-05 11:38:47 -0500

    finalized books controller

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Book: require("./book")
+    // WorkoutPlan: require("./workoutplan"),
+    // Workout: require("./workout")
+  };
